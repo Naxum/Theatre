a07b419905dbf2a3a5d537dd9b672e78991ff967
Naxum <jake@naxite.com>
2017-07-07 15:30:58 -0400

    Initial Commit

---
+++ Theatre/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Theatre
+//
+//  Created by Jacob Sawyer on 7/7/17.
+//  Copyright © 2017 Jake Sawyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
